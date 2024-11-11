88696ae34b4d30d4b927c4f1a4b73c2fd2fc7dec
Samara Souza <samyesouza13@gmail.com>
2024-11-11 09:48:25 -0300

    Creating club routes and names for club.controller async functions

---
+++ apps/backend/src/controllers/club.controller.mjs
@@ -0,0 +1,11 @@
+import prismaClient from '../utils/prismaClient.mjs';
+
+class ClubController{
+    async createClub(req,res){}
+    async getClubs(req,res){}
+    async getClubById(req,res){}
+    async updateClub(req,res){}
+    async deleteClub(req,res){}
+}
+
+export default ClubController;
