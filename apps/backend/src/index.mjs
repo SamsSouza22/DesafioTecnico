3255bf5dede97c7c860df541a6aa407f14c2eeb1
Samara Souza <samyesouza13@gmail.com>
2024-11-11 20:44:31 -0300

    Archiving logs using morgan

--- apps/backend/src/index.mjs
+++ apps/backend/src/index.mjs
@@ -1,13 +1,18 @@
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
 
 import { privateRouter, publicRouter } from './routes/routers.mjs';
 import './routes/user.router.mjs';
 import './routes/club.router.mjs';
+
 const server = express();
+const accessLogStream = fs.createWriteStream(path.join(process.cwd(), 'logs', 'access.log'), { flags: 'a' });
 
 server.use(helmet());
+server.use(morgan('combined', { stream: accessLogStream }));
 server.use(morgan('dev'));
 server.use(express.json());
 server.use(publicRouter);
